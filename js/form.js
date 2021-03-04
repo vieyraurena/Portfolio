85378598030a25174600da46c95acbdb483a3977
Vieyra <vieyraurenabravo@gmail.com>
2021-03-03 22:09:40 -0600

    iteracion/add/fuctionForm

---
+++ js/form.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+const required = document.querySelectorAll('.required');
+
+const h4 = document.querySelector('h4');
+const warning = document.createElement('h5');
+const check = document.createElement('p');
+check.innerHTML = '';
+
+const formValidation = () => {
+  required.forEach((input) => {
+    if (input.value === '') {
+      input.classList.add('red');
+      warning.innerHTML = `Ocurrio un error, verifica los siguientes campos ${required}`; 
+      h4.appendChild(warning); 
+    } else {
+      input.classList.remove('red');
+      check.innerHTML = 'Su formulario fue enviado';
+      check.classList.add('green');
+      form.appendChild(check);
+    }
+  })
+}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  formValidation();
+  event.target.reset();
+});
